b53649340202fc3d7cd9fe38f9280d8a3da52082
Get Win Harris <94005095+getwinharris@users.noreply.github.com>
2022-03-31 21:27:12 +0530

    Update route.test.ts

--- test/route.test.ts
+++ test/route.test.ts
@@ -3,10 +3,10 @@ import { Token, WETH, ChainId, Pair, TokenAmount, Route, ETHER } from '../src'
 describe('Route', () => {
   const token0 = new Token(ChainId.MAINNET, '0x0000000000000000000000000000000000000001', 18, 't0')
   const token1 = new Token(ChainId.MAINNET, '0x0000000000000000000000000000000000000002', 18, 't1')
-  const weth = WETH[ChainId.MAINNET]
+  const weth = BAPx[ChainId.MAINNET]
   const pair_0_1 = new Pair(new TokenAmount(token0, '100'), new TokenAmount(token1, '200'))
-  const pair_0_weth = new Pair(new TokenAmount(token0, '100'), new TokenAmount(weth, '100'))
-  const pair_1_weth = new Pair(new TokenAmount(token1, '175'), new TokenAmount(weth, '100'))
+  const pair_0_bapx = new Pair(new TokenAmount(token0, '100'), new TokenAmount(weth, '100'))
+  const pair_1_bapx = new Pair(new TokenAmount(token1, '175'), new TokenAmount(weth, '100'))
 
   it('constructs a path from the tokens', () => {
     const route = new Route([pair_0_1], token0)
